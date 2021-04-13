78e3e2fb29cff5c923adb6d636eae392f9225557
SAMO <samorj1783@gmail.com>
2021-04-13 02:45:28 -0500

    Mapa web

---
+++ data/CAPITAL_8_PROVINCIAS_3.js
@@ -0,0 +1,15 @@
+var json_CAPITAL_8_PROVINCIAS_3 = {
+"type": "FeatureCollection",
+"name": "CAPITAL_8_PROVINCIAS_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMOBJ": "SAN LORENZO" }, "geometry": { "type": "Point", "coordinates": [ -76.55452596961581, -4.830747914625814 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "YURIMAGUAS" }, "geometry": { "type": "Point", "coordinates": [ -76.104425662529422, -5.895228773958971 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "IQUITOS" }, "geometry": { "type": "Point", "coordinates": [ -73.244360363848983, -3.749265391085499 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "REQUENA" }, "geometry": { "type": "Point", "coordinates": [ -73.856352391099009, -5.063597909805296 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "CONTAMANA" }, "geometry": { "type": "Point", "coordinates": [ -75.005970991769601, -7.349227048104381 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "NAUTA" }, "geometry": { "type": "Point", "coordinates": [ -73.575459802708679, -4.506616338765647 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "SAN ANTONIO DEL ESTRECHO" }, "geometry": { "type": "Point", "coordinates": [ -72.670755376144683, -2.448415535957954 ] } },
+{ "type": "Feature", "properties": { "NOMOBJ": "CABALLO COCHA" }, "geometry": { "type": "Point", "coordinates": [ -70.516808000064714, -3.905980997626276 ] } }
+]
+}
